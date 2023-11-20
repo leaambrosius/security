c4aa195200246557ec09121079355e95999fba00
Alicja Niewiadomska <micek2000@gmail.com>
2023-11-20 18:46:09 +0000

    Add client

---
+++ src/App/Client/ConnectionB.java
@@ -0,0 +1,12 @@
+package App.Client;
+
+public class ConnectionB {
+    public static void main(String[] args) {
+        String serverPort = "12345";
+        String serverIP = "localhost";
+        String localPort = "54322";
+        Peer user = new Peer("B", serverIP, serverPort, localPort);
+        user.announceToServer();
+        user.connectToPeer("A");
+    }
+}
